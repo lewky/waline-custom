ccaf462a8f6c4e2b0a161221c9581b83de0bb4af
Lewis.Liu <1019175915@qq.com>
2021-02-16 13:04:51 +0800

    修改index.js

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const Waline = require('@waline/vercel');
+const Application = require('@waline/vercel');
 
-module.exports = Waline({
-  forbiddenWords: [
-    'Ï°½üÆ½',
-    'Ã«Ôó¶«'
-  ]
-});
-
-module.exports = Waline({
-  disallowIPList: [
-    '8.8.8.8',
-    '4.4.4.4'
-  ]
-});
-
-module.exports = Waline({
+module.exports = Application({
   async postSave(comment, pComment) {
 	console.log(`comment ${comment.objectId} has been saved! Now send mail...`);
     await mailto({
