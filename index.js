a34c9954dd89c6e5bdb661591c84e78d3d1a7891
Lewis.Liu <1019175915@qq.com>
2021-02-16 13:27:25 +0800

    还原初始配置

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const Application = require('@waline/vercel');
 
 module.exports = Application({
-  async postSave(comment, pComment) {
-	console.log(`comment ${comment.objectId} has been saved! Now send mail...`);
-    await mailto({
-      mail: pComment.mail,
-      text: `${comment.nick} replied your comment!`
-    });
+  async postSave(comment) {
+    //do what ever you want after save comment
   }
 });
