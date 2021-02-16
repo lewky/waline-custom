aadcdc2afad25e5fbef5e87a05400466287b1ddc
Lewis.Liu <1019175915@qq.com>
2021-02-16 11:49:41 +0800

    添加违禁词和IP禁止名单

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const Application = require('@waline/vercel');
+const Waline = require('@waline/vercel');
 
-module.exports = Application({
+module.exports = Waline({
+  forbiddenWords: [
+    'Ï°½üÆ½',
+    'Ã«Ôó¶«'
+  ],
+  disallowIPList: [
+    '8.8.8.8',
+    '4.4.4.4'
+  ],
   async postSave(comment) {
     //do what ever you want after save comment
   }
